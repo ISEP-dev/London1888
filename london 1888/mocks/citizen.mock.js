dd21329af7c70c8677397bb7b9aafb8a408b0408
Perrault <lucas.perrault@hotmail.com>
2020-11-17 18:22:31 +0100

    ADD citizen mock

---
+++ london 1888/mocks/citizen.mock.js
@@ -0,0 +1,40 @@
+export const VICTIM = {
+    id: 0,
+    name: "the_victim",
+    posX: 10,
+    posY: 10,
+    isVictim: true,
+};
+
+export const CLOSEST_CITIZEN = {
+    id: 1,
+    name: "closest_citizen",
+    posX: 10,
+    posY: 9,
+    isVictim: false,
+}
+
+export const SAME_CLOSEST_CITIZEN = {
+    id: 2,
+    name: "same_closest_citizen",
+    posX: 10,
+    posY: 9,
+    isVictim: false,
+}
+
+export const BASIC_CITIZEN = {
+    id: 3,
+    name: "basic_citizen",
+    posX: 200,
+    posY: 300,
+    isVictim: false,
+};
+
+export const OTHER_BASIC_CITIZEN = {
+    id: 4,
+    name: "other_basic_citizen",
+    posX: 300,
+    posY: 700,
+    isVictim: false,
+};
+
