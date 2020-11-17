8896c764fbdd2c245236c87f91777387bdb3a552
Perrault <lucas.perrault@hotmail.com>
2020-11-17 18:22:23 +0100

    FIX math helper

--- london 1888/math.helpers.js
+++ london 1888/math.helpers.js
@@ -1,6 +1,6 @@
 class MathHelpers {
     static getDistanceBetween(a, b) {
-        Math.sqrt(Math.pow((a.posX - b.posX), 2) + Math.pow((a.posY - b.posY), 2))
+        return Math.sqrt(Math.pow((a.posX - b.posX), 2) + Math.pow((a.posY - b.posY), 2))
     }
 }
 
