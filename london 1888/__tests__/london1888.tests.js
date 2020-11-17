e4fdbe58ad67db3c0ae554c5f18e2e578d47b9a0
Perrault <lucas.perrault@hotmail.com>
2020-11-17 18:36:13 +0100

    ADD test for london1888

---
+++ london 1888/__tests__/london1888.tests.js
@@ -0,0 +1,15 @@
+import {BASIC_CITIZEN, CLOSEST_CITIZEN, OTHER_BASIC_CITIZEN, SAME_CLOSEST_CITIZEN, VICTIM} from "../mocks/citizen.mock";
+import {getLondon1888} from "../London1888";
+
+describe('London 1888', () => {
+    it('Get all closest citizen',() => {
+        const citizens = [CLOSEST_CITIZEN, SAME_CLOSEST_CITIZEN, BASIC_CITIZEN, OTHER_BASIC_CITIZEN]
+
+        const res = getLondon1888().getClosestCitizensFromVictim(VICTIM, citizens)
+        expect(res).toEqual([CLOSEST_CITIZEN, SAME_CLOSEST_CITIZEN])
+    })
+    it('Get null when no citizens',() => {
+        const res = getLondon1888().getClosestCitizensFromVictim(VICTIM, [])
+        expect(res).toEqual([])
+    })
+})
