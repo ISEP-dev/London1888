10ce7b67b6b6fb5b9c875785c8dc8158c39e5b2d
Perrault <lucas.perrault@hotmail.com>
2020-11-17 16:38:08 +0100

    MOD routes

--- london 1888/app.js
+++ london 1888/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import Dal from "./dal";
+import london1888 from "./London1888";
 
 const app = express()
 
@@ -25,27 +26,27 @@ app.post('/victim/:name/:posX/:posY', (req, res) => {
 
 })
 
-/**
- * @returns the closest citizen from the victim's location
- * */
 app.get('/getJack', async (req, res) => {
-    /* Implement the formula returns the dist between A and B*/
-    const dal = new Dal()
-    try {
-        const victim = await dal.getVictimAsync();
-    } catch (e) {
+    const victim = await london1888.getVictimAsync();
+    const citizens = await london1888.getCitizensAsync();
 
+    if (!victim || !citizens.length) {
+        return res.status(404).end();
     }
+
+    const closestCitizens = london1888.getClosestCitizensFromVictim(victim, citizens);
+    if (closestCitizens.length > 1) {
+        return res.status(409).end();
+    }
+
+    return res.status(200).set({ 'Content-Type': 'application/json' }).json(closestCitizens[0]);
 })
 
 app.delete('/evidences', async (req, res) => {
     const dal = new Dal();
-    try {
-        await dal.resetTableAsync();
-        res.status(200).end();
-    } catch (e) {
-        res.status(400).set({ 'Content-Type': 'application/json' }).json(e)
-    }
+
+    await dal.resetTableAsync();
+    res.status(204).end();
 })
 
 export default app
