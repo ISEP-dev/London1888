9dcad07073235166aff17cb69615a6008e8a5585
bastiengrignon <basgrignon@gmail.com>
2020-11-21 15:59:35 +0100

    ADD london1888 tests

--- london 1888/__tests__/london1888.tests.js
+++ london 1888/__tests__/london1888.tests.js
@@ -1,15 +1,27 @@
-import {BASIC_CITIZEN, CLOSEST_CITIZEN, OTHER_BASIC_CITIZEN, SAME_CLOSEST_CITIZEN, VICTIM} from "../mocks/citizen.mock";
+import {
+    BASIC_CITIZEN,
+    CLOSEST_CITIZEN,
+    OTHER_BASIC_CITIZEN,
+    SAME_CLOSEST_CITIZEN,
+    VICTIM
+} from "../mocks/citizen.mock";
 import {getLondon1888} from "../london1888";
 import ConflictError from "../errors/conflict.error";
 import NotfoundError from "../errors/notfound.error";
 
 const mockGetAllCitizensAsync = jest.fn();
 const mockGetVictimAsync = jest.fn();
+const mockCreateCitizenAsync = jest.fn();
+const mockCreateVictimAsync = jest.fn();
+const mockNumberOfVictimAsync = jest.fn();
 
 jest.mock('../dal.js', () => {
     return jest.fn().mockImplementation(() => ({
         getAllCitizenAsync: mockGetAllCitizensAsync,
         getVictimAsync: mockGetVictimAsync,
+        createCitizenAsync: mockCreateCitizenAsync,
+        createVictimAsync: mockCreateVictimAsync,
+        numberOfVictimAsync: mockNumberOfVictimAsync
     }))
 })
 
@@ -24,6 +36,7 @@ describe('London 1888', () => {
             expect(true).toBeFalsy();
         }
     });
+
     it('Get all citizen : Throw not found', async () => {
         try {
             const expectedResponse = [];
@@ -44,6 +57,7 @@ describe('London 1888', () => {
             expect(true).toBeFalsy();
         }
     });
+
     it('Get victim : Throw not found', async () => {
         try {
             const expectedResponse = undefined;
@@ -54,7 +68,7 @@ describe('London 1888', () => {
         }
     });
 
-    it('Get closest citizen : done', () => {
+    it('Get closest citizen : Done', () => {
         try {
             const citizens = [CLOSEST_CITIZEN, BASIC_CITIZEN, OTHER_BASIC_CITIZEN];
             const res = getLondon1888().getClosestCitizensFromVictim(VICTIM, citizens)[0];
@@ -63,6 +77,7 @@ describe('London 1888', () => {
             expect(true).toBeFalsy();
         }
     });
+
     it('Get closest citizen : Throw conflict', () => {
         try {
             const citizens = [CLOSEST_CITIZEN, SAME_CLOSEST_CITIZEN, BASIC_CITIZEN, OTHER_BASIC_CITIZEN]
@@ -71,4 +86,53 @@ describe('London 1888', () => {
             expect(e).toBeInstanceOf(ConflictError);
         }
     });
+
+    it('Create a citizen : Done', async () => {
+        mockCreateCitizenAsync.mockReturnValue(BASIC_CITIZEN.id)
+        try {
+            const citizen = await getLondon1888().createCitizenAsync(
+                BASIC_CITIZEN.name,
+                BASIC_CITIZEN.posX,
+                BASIC_CITIZEN.posY,
+                0)
+            expect(citizen).toEqual(BASIC_CITIZEN)
+        } catch (e) {
+            expect(true).toBeFalsy()
+        }
+    });
+
+    it('Create a victim : Done', async () => {
+        mockCreateVictimAsync.mockReturnValue(VICTIM.id)
+        try {
+            const victim = await getLondon1888().createVictimAsync(
+                VICTIM.name,
+                VICTIM.posX,
+                VICTIM.posY,
+                1)
+            expect(victim).toEqual(VICTIM)
+        } catch (e) {
+            expect(true).toBeFalsy()
+        }
+    });
+
+    it('Check if it has already a victim : Done', async () => {
+        mockNumberOfVictimAsync.mockReturnValue(0)
+        try {
+            await getLondon1888().checkIfHasAlreadyAVictimAsync()
+            expect(true).toBeTruthy()
+        } catch (e) {
+            expect(true).toBeFalsy()
+        }
+    });
+
+    it('Check if it has already a victim : Throw exception', async () => {
+        mockNumberOfVictimAsync.mockReturnValue(1)
+        try {
+            await getLondon1888().checkIfHasAlreadyAVictimAsync()
+            expect(true).toBeFalsy()
+        } catch (e) {
+            expect(e).toBeInstanceOf(ConflictError);
+        }
+    });
+
 })
